b48dff592d32b7f0247fdb7792c7111bfdc521fa
kreid333 <kai.j.reid@gmail.com>
2021-03-26 01:31:49 -0400

    added dependencies to layout

--- libertyccc/src/components/Layout.jsx
+++ libertyccc/src/components/Layout.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import Navbar from "./Navbar/Navbar";
-import Footer from "./Footer/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@fontsource/montserrat";
-import "../styles/global.css";
+import React from "react"
+import Navbar from "./Navbar/Navbar"
+import Footer from "./Footer/Footer"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "jquery/dist/jquery.min.js"
+import "popper.js/dist/popper.min"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "@fontsource/montserrat"
+import "../styles/global.css"
 
 const Layout = ({ children }) => {
   return (
