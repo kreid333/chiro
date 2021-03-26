49cccdfd320ab945eb7990e8c049daf7c5f4465a
kreid333 <kai.j.reid@gmail.com>
2021-03-26 01:57:07 -0400

    changed layout imports v2

--- libertyccc/src/components/Layout.jsx
+++ libertyccc/src/components/Layout.jsx
@@ -2,8 +2,9 @@ import React from "react"
 import Navbar from "./Navbar/Navbar"
 import Footer from "./Footer/Footer"
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
+import "jquery/dist/jquery.min.js"
+import "popper.js/dist/popper.min"
+import "bootstrap/dist/js/bootstrap.min.js"
 import "@fontsource/montserrat"
 import "../styles/global.css"
 
