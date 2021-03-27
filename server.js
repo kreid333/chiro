8696d9bc0c402462b5f023b0d972aabc2fbb8ad4
kreid333 <kai.j.reid@gmail.com>
2021-03-27 17:09:03 -0400

    trying to serve static pages through express for deployment and communicating with backend

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const apiRoutes = require("./controller/apiController");
 app.use(apiRoutes);
 
 // BUILD FOLDER STATIC
-app.use(express.static("client/build"));
+app.use(express.static("libertyccc/public"));
 
 // ROUTE TO SERVER
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
